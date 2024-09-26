d59ce06916652aa1ca5b8325ed31ceb09f014f54
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2024-09-26 18:05:13 +0800

    第3章 3.1 Reading and writing data with JDBC

--- ch03/taco-cloud-jdbc/src/main/java/tacos/web/OrderController.java
+++ ch03/taco-cloud-jdbc/src/main/java/tacos/web/OrderController.java
@@ -46,6 +46,7 @@ public class OrderController {
             return "orderForm";
         }
         orderRepo.save(order);
+        log.info("Processing order: {}", order);
         sessionStatus.setComplete();
 
         return "redirect:/";
